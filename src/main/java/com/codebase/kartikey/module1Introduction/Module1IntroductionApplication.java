d4a017dcf4c84dcc80a30cae011043ff399a449e
CodingWithKartikey <krtikeytiwari12@gmail.com>
2025-11-24 01:21:24 +0530

    Autowired & Component annotations & CommandLineRunner

--- src/main/java/com/codebase/kartikey/module1Introduction/Module1IntroductionApplication.java
+++ src/main/java/com/codebase/kartikey/module1Introduction/Module1IntroductionApplication.java
@@ -1,13 +1,23 @@
 package com.codebase.kartikey.module1Introduction;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Module1IntroductionApplication {
+public class Module1IntroductionApplication implements CommandLineRunner {
+
+	@Autowired
+	PaymentService paymentService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(Module1IntroductionApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		paymentService.pay();
 	}
 
 }
---
+++ src/main/java/com/codebase/kartikey/module1Introduction/PaymentService.java
@@ -0,0 +1,11 @@
+package com.codebase.kartikey.module1Introduction;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class PaymentService {
+
+    public void pay() {
+        System.out.println("Paying...");
+    }
+}
