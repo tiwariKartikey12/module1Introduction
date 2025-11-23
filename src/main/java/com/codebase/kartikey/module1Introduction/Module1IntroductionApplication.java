cc4a72082a88f286d3da1d7106661d695ab38da0
CodingWithKartikey <krtikeytiwari12@gmail.com>
2025-11-24 01:12:47 +0530

    Initial Files

---
+++ src/main/java/com/codebase/kartikey/module1Introduction/Module1IntroductionApplication.java
@@ -0,0 +1,13 @@
+package com.codebase.kartikey.module1Introduction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Module1IntroductionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Module1IntroductionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codebase/kartikey/module1Introduction/Module1IntroductionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codebase.kartikey.module1Introduction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Module1IntroductionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
