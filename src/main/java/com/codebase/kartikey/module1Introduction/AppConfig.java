c427ac06452b2411762c468f20f079adcfabc02d
CodingWithKartikey <krtikeytiwari12@gmail.com>
2025-11-24 02:02:27 +0530

    Bean Scope Annotation: When scope annotation not used then same hashcode(reference)

--- src/main/java/com/codebase/kartikey/module1Introduction/AppConfig.java
+++ src/main/java/com/codebase/kartikey/module1Introduction/AppConfig.java
@@ -2,11 +2,13 @@ package com.codebase.kartikey.module1Introduction;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
 
 @Configuration
 public class AppConfig {
 
     @Bean
+    //@Scope("prototype")
     PaymentService paymentService() {
         //more logic
         return new PaymentService();
