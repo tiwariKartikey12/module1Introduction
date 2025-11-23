57fbdc343d3cbf4fc9a43715e9d5c0bb394cea7c
CodingWithKartikey <krtikeytiwari12@gmail.com>
2025-11-24 01:51:14 +0530

    Bean Lifecycles Methods: PostConstructor & PreDestroy Annotation

--- src/main/java/com/codebase/kartikey/module1Introduction/PaymentService.java
+++ src/main/java/com/codebase/kartikey/module1Introduction/PaymentService.java
@@ -1,5 +1,7 @@
 package com.codebase.kartikey.module1Introduction;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -7,5 +9,15 @@ public class PaymentService {
 
     public void pay() {
         System.out.println("Paying...");
+    }
+
+    @PostConstruct
+    public void afterInit() {
+        System.out.println("Before Paying...");
+    }
+
+    @PreDestroy
+    public void beforeDestroy() {
+        System.out.println("After Payment is done");
     }
 }
